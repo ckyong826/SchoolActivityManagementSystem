6ba2f7b3c5232d548ef67e6b191a330a78239796
Ck Yong <118044421+ckyong826@users.noreply.github.com>
2024-06-25 19:17:26 +0800

    logic fixed

--- client/src/public/components/modalComponent/widget/ModalCompletionComponent.jsx
+++ client/src/public/components/modalComponent/widget/ModalCompletionComponent.jsx
@@ -8,20 +8,22 @@ import axiosClient from '../../../../axios-client';
 import Notification from './useNotification';
 
 const ModalCompletionComponent = (props) => {
-  const user = useGetCurrentUser();
-  console.log(user?.userID)
+  const [loading, setLoading] = React.useState(false);
+  console.log(props.user?.userID)
   const handleRegister = async () => {
+    setLoading(true);
     const payload = {
-      userID: user?.userID,
+      userID: props.user?.userID,
       activityID: props.prop.activityID,
-      regDate : new Date().toISOString(),
     };
     try {
-      await axios.post('/registration', payload);
+      await axiosClient.post('/registration', payload);
       console.log('Joined activity successfully');
       props.setStep(3);
+      setLoading(false);
     } catch (error) {
       console.error('Error joining activity:', error.response?.data || error.message);
+      setLoading(false);
     }
   }
   return (
